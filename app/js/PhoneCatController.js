187822de644a5fce39b6860bf087e33540d3035a
Ryingyren@thoughtworks.com <yren@thoughtworks.com>
2015-12-11 11:26:20 +0800

    use angular controller to show phones.

---
+++ app/js/PhoneCatController.js
@@ -0,0 +1,10 @@
+app.controller('PhoneCatController', function($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S',
+      'description': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM with Wi-Fi',
+      'description': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM',
+      'description': 'The Next, Next Generation tablet.'}
+  ];
+});
